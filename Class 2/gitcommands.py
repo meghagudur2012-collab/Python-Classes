33b6cb6b89213bf9298e4243afb7afb80b50e5d7
Megha <meghagudur2012@gmail.com>
2026-06-21 18:25:00 -0400

    practice basic python

--- Class 2/gitcommands.py
+++ Class 2/gitcommands.py
@@ -2,3 +2,15 @@ print("MeghaG")
 #git add .
 #git commit -m "learning git commands"
 # git push
+
+print(1)
+print(12+10*222/45+90-78*34/21)
+print("akash")
+print("Megha \n" *10)
+print("Megha"+" Gudur")
+print("12" + "10")
+print("my name is 'Megha' ")
+print(1, 2, 3, 4, 5, "Megha", 12.4, "sum 10+20:", 30)
+#arithmetic operator : +,-,*,/,%(modulus,remainders),**(power, exponent)
+
+print("129 + 130 is", 100+200)
