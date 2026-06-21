38cb10cac686a266701c4dd6c216323ef8e615a1
Megha <meghagudur2012@gmail.com>
2026-06-21 18:45:54 -0400

    Moved file

---
+++ Class 2/practice.py
@@ -0,0 +1,24 @@
+print("My name is Megha\n")
+print("I am learning python\n")
+print("Python is fun\n")
+
+print(10 + 20)
+print(50 - 15)
+print(12 * 5)
+print(100 / 4)
+
+firstName = "Megha "
+lastName = "Gudur"
+print(firstName + lastName)
+
+name = "Megha"
+age = "13"
+city = "Cary"
+print("Name: " + name)
+print("Age: " + age)
+print("City: " + city)
+
+print("Megha")
+print(10 + 20)
+print("10" + "20")
+print("Python\nGithub")
