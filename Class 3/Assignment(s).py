0d590280123bf3c68a311a6eca4cf1a9a0eb3c32
Megha <meghagudur2012@gmail.com>
2026-06-22 15:05:44 -0400

    learning git commands

---
+++ Class 3/Assignment(s).py
@@ -0,0 +1,13 @@
+costPrice = 400
+sellPrice = 500
+profit = sellPrice - costPrice
+print("The profit from selling is",profit)
+
+english_Marks = 90
+math_Marks = 90
+science_Marks = 90
+total_Marks = english_Marks + math_Marks + science_Marks
+percentage = (total_Marks / 300) * 100
+
+print(total_Marks,"/300")
+print(percentage)
---
+++ Class 3/newInfo.py
@@ -0,0 +1,12 @@
+x = 10 
+y = 20.3
+sum = x + y
+the_great_sum = "hi" #snake variable method
+theGreatSum = "hi" #camel variable  method
+leftOutAmount = x-y
+
+
+print("value of x is {x}")
+print("value of y is {y}")
+print("the sum of {x} and {y} is {sum}")
+print(25**0.5)#how to find the square root
