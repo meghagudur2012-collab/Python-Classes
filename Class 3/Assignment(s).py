e20819ec4b1b4b24e36731182519833301ce1455
Megha <meghagudur2012@gmail.com>
2026-06-22 15:27:12 -0400

    learning git commands

--- Class 3/Assignment(s).py
+++ Class 3/Assignment(s).py
@@ -1,7 +1,7 @@
 costPrice = 400
 sellPrice = 500
 profit = sellPrice - costPrice
-print("The profit from selling is",profit)
+print(f"The profit from selling is {profit}")
 
 english_Marks = 90
 math_Marks = 90
@@ -9,5 +9,28 @@ science_Marks = 90
 total_Marks = english_Marks + math_Marks + science_Marks
 percentage = (total_Marks / 300) * 100
 
-print(total_Marks,"/300")
+print(f"{total_Marks}/300")
 print(percentage)
+
+principleAmount = 100000
+rateOfInterest = 2.5
+timeInYears = 3
+simpleInterest = (principleAmount * rateOfInterest * timeInYears) / 100
+
+print(simpleInterest)
+
+monthlySal = 6000
+annualSal = monthlySal * 12
+
+print(annualSal)
+
+
+price = 8.99
+quantity = 5
+totalBill = price * quantity
+GST = 0.18 * totalBill
+finalAmount = totalBill + GST
+
+print(totalBill)
+print(GST)
+print(finalAmount)
